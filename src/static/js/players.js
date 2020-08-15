2952a1e434abff1fed7be6ced868c038f1256f8b
Dongseon Lee <eastline@maycan.co.kr>
2020-08-15 23:42:28 +0900

    #6.2 Player Board part One

---
+++ src/static/js/players.js
@@ -0,0 +1,16 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.handlePlayerUpdate = void 0;
+
+var handlePlayerUpdate = function handlePlayerUpdate(_ref) {
+  var sockets = _ref.sockets;
+  return console.log(sockets);
+};
+
+exports.handlePlayerUpdate = handlePlayerUpdate;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZha2VfZWZiNGM2MTguanMiXSwibmFtZXMiOlsiaGFuZGxlUGxheWVyVXBkYXRlIiwic29ja2V0cyIsImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBTyxJQUFNQSxrQkFBa0IsR0FBRyxTQUFyQkEsa0JBQXFCO0FBQUEsTUFBR0MsT0FBSCxRQUFHQSxPQUFIO0FBQUEsU0FBaUJDLE9BQU8sQ0FBQ0MsR0FBUixDQUFZRixPQUFaLENBQWpCO0FBQUEsQ0FBM0IiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgaGFuZGxlUGxheWVyVXBkYXRlID0gKHsgc29ja2V0cyB9KSA9PiBjb25zb2xlLmxvZyhzb2NrZXRzKTtcbiJdfQ==
+},{}]},{},[1])
