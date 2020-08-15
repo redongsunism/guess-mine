c8e74f042db570cae984c45a344ad9d7e2df299a
Dongseon Lee <eastline@maycan.co.kr>
2020-08-15 18:08:57 +0900

    #6.0 Realtime Painting part One

--- src/socketController.js
+++ src/socketController.js
@@ -12,8 +12,16 @@ const socketController = (socket) => {
     broadcast(events.disconnected, { nickname: socket.nickname });
   });
 
-  socket.on(events.sendMsg, ({ message }) => {
-    broadcast(events.newMsg, { message, nickname: socket.nickname });
+  socket.on(events.sendMsg, ({ message }) =>
+    broadcast(events.newMsg, { message, nickname: socket.nickname })
+  );
+
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+
+  socket.on(events.strokePath, ({ x, y }) => {
+    broadcast(events.strokedPath, { x, y });
   });
 };
 
